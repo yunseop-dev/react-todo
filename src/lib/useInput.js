29edc66de0d9a5e698bbd81d9a51dec6ee76a8f2
Yunseop Kim <yunseop.kim@actionpower.kr>
2021-08-06 11:52:29 +0900

    add libs (useInput, useLocalStorage, usePromise)

---
+++ src/lib/useInput.js
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+const useInput = (initValue) => {
+    const [value, setValue] = useState(initValue)
+
+    const onChange = (e) => {
+        setValue(e.target.value)
+    }
+
+    return {
+        value, onChange
+    }
+}
+
+export default useInput
---
+++ src/lib/usePromise.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+
+const usePromise = (promiseCreator, deps) => {
+    const [loading, setLoading] = useState(false)
+    const [resolved, setResolved] = useState(null)
+    const [error, setError] = useState(null)
+
+
+    useEffect(() => {
+        const process = async () => {
+            setLoading(true)
+            try {
+                const resolved = await promiseCreator()
+                setResolved(resolved)
+            } catch (e) {
+                setError(e)
+            }
+            setLoading(false)
+        }
+
+        process()
+    }, deps)
+
+    return [loading, resolved, error]
+}
+
+export default usePromise
