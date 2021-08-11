7b54ad6c5967dbafc49869002ff040c679b117d5
Yunseop Kim <yunseop.kim@actionpower.kr>
2021-08-11 11:38:45 +0900

    클라이언트 추가

---
+++ src/lib/client.js
@@ -0,0 +1,35 @@
+const localStorageKey = 'token'
+export default async function client(endpoint, { body, ...customConfig } = {}) {
+    const token = window.localStorage.getItem(localStorageKey)
+    const headers = { 'content-type': 'application/json' }
+    if (token) {
+        headers.Authorization = token.replace(/"/g, '')
+    }
+    const config = {
+        method: body ? 'POST' : 'GET',
+        ...customConfig,
+        headers: {
+            ...headers,
+            ...customConfig.headers,
+        },
+    }
+    if (body) {
+        config.body = JSON.stringify(body)
+    }
+    const response = await window
+        .fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, config)
+    if (response.status === 401) {
+        logout()
+        window.location.assign(window.location)
+        return
+    }
+    if (response.ok) {
+        return response.json()
+    } else {
+        const errorMessage = await response.text()
+        return Promise.reject(new Error(errorMessage))
+    }
+}
+function logout() {
+    window.localStorage.removeItem(localStorageKey)
+}
