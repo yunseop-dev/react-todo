e50dfa8714f1891c6e33dfdcb2682eade510b6db
Yunseop Kim <yunseop.kim@actionpower.kr>
2021-08-19 16:37:55 +0900

    lru cache추가

--- src/lib/createRequestSaga.js
+++ src/lib/createRequestSaga.js
@@ -1,14 +1,41 @@
 import { call, put } from "redux-saga/effects"
 import { setFetchStatus } from "../modules/fetchStatus"
 import { FETCH_STATUS } from '../constants'
+import lruCache from 'lru-cache';
 
-export default function createRequestSaga(type, request) {
+const apiCache = new lruCache({
+    max: 500, maxAge: 1000 * 60 * 2
+})
+
+export function getApiCacheKey(actionType, { params }) {
+    const prefix = `${actionType}_`;
+    const keys = new URLSearchParams(params);
+    return prefix + keys;
+}
+
+export function deleteApiCache(actionType) {
+    let keys = apiCache.keys();
+    if (actionType) {
+        keys = keys.filter(key => key.includes(actionType));
+    }
+    for (const key of keys) {
+        apiCache.del(key);
+    }
+}
+
+export default function createRequestSaga(type, request, cacheable = false) {
     const SUCCESS = `${type}_SUCCESS`
     const FAILURE = `${type}_FAILURE`
     return function* (action) {
         yield put(setFetchStatus({ requestType: type, status: FETCH_STATUS.REQUEST }))
         try {
-            const response = yield call(request, action.payload)
+            const cacheKey = getApiCacheKey(type, { params: action.payload })
+            const isFromCache = apiCache.has(cacheKey)
+            const response = (isFromCache && cacheable) ? apiCache.get(cacheKey) : yield call(request, action.payload)
+            if (!isFromCache) {
+                apiCache.set(cacheKey, response)
+            }
+
             yield put(setFetchStatus({ requestType: type, status: FETCH_STATUS.SUCCESS }))
             yield put({
                 type: SUCCESS, payload: response, params: action.payload
