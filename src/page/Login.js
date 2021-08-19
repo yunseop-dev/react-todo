8633ebc0bb4292e8976ccb603377ec75f6d2f3f9
Yunseop Kim <yunseop.kim@actionpower.kr>
2021-08-19 16:40:37 +0900

    로그인 시 리다이렉션 로직 추가

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,8 +1,9 @@
 import useInput from "../lib/useInput"
 import { useHistory } from "react-router-dom"
 import { useDispatch } from "react-redux"
-import { login } from '../modules/user'
-import React, { useCallback } from "react"
+import { login, Types } from '../modules/user'
+import React, { useCallback, useEffect } from "react"
+import useFetchInfo from "../lib/useFetchInfo"
 
 const Login = () => {
     const history = useHistory()
@@ -10,15 +11,22 @@ const Login = () => {
     const { value: password, onChange: onChangePassword } = useInput('')
     const dispatch = useDispatch()
     const onLogin = useCallback((data) => dispatch(login(data)), [dispatch])
+    const { isFetched } = useFetchInfo(Types.LOGIN)
     const onSubmit = async (e) => {
         e.preventDefault()
         try {
             onLogin({ email, password })
-            history.push('/')
         } catch (error) {
             alert(error)
         }
     }
+
+    useEffect(() => {
+        if (isFetched) {
+            history.push('/')
+        }
+    }, [history, isFetched])
+
     return <form onSubmit={onSubmit}>
         <p>
             <label htmlFor='email'>
