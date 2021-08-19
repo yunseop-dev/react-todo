1dd44dc3335e7d87aea3ee77a0ac5e48ee433a89
Yunseop Kim <yunseop.kim@actionpower.kr>
2021-08-19 10:34:40 +0900

    유저 리듀서 코드 리팩토링

--- src/modules/user.js
+++ src/modules/user.js
@@ -1,71 +1,100 @@
-import { takeLatest } from 'redux-saga/effects'
+import { all, takeLatest } from 'redux-saga/effects'
 import * as api from '../lib/api'
 import createRequestSaga from '../lib/createRequestSaga'
 
-const GET_USER = 'user/GET_USER'
-const GET_USER_SUCCESS = 'user/GET_USER_SUCCESS'
-export const getUser = () => ({ type: GET_USER })
-export const getUserSaga = createRequestSaga(GET_USER, api.getUser)
+export const Types = {
+    GET_USER: 'user/GET_USER',
+    GET_USER_SUCCESS: 'user/GET_USER_SUCCESS',
+    LOGIN: 'user/LOGIN',
+    LOGIN_SUCCESS: 'user/LOGIN_SUCCESS',
+    LOGOUT: 'user/LOGOUT',
+    LOGOUT_SUCCESS: 'user/LOGOUT_SUCCESS',
+    REGISTER: 'user/REGISTER',
+    REGISTER_SUCCESS: 'user/REGISTER_SUCCESS',
+    UPDATE_USER: 'user/UPDATE_USER',
+    UPDATE_USER_SUCCESS: 'user/UPDATE_USER_SUCCESS',
+    UPLOAD_AVATAR: 'user/UPLOAD_AVATAR',
+    UPLOAD_AVATAR_SUCCESS: 'user/UPLOAD_AVATAR_SUCCESS',
+    REMOVE_USER: 'user/REMOVE_USER',
+    REMOVE_USER_SUCCESS: 'user/REMOVE_USER_SUCCESS',
+}
 
-const LOGIN = 'user/LOGIN'
-const LOGIN_SUCCESS = 'user/LOGIN_SUCCESS'
-export const login = ({ email, password }) => ({ type: LOGIN, payload: { email, password } })
-export const loginSaga = createRequestSaga(LOGIN, api.login)
+export const getUser = () => ({ type: Types.GET_USER })
+export const getUserSaga = createRequestSaga(Types.GET_USER, api.getUser)
 
-const LOGOUT = 'user/LOGOUT'
-export const LOGOUT_SUCCESS = 'user/LOGOUT_SUCCESS'
-export const logout = () => ({ type: LOGOUT })
-export const logoutSaga = createRequestSaga(LOGOUT, api.logout)
+export const login = ({ email, password }) => ({ type: Types.LOGIN, payload: { email, password } })
+export const loginSaga = createRequestSaga(Types.LOGIN, api.login)
 
-const REGISTER = 'user/REGISTER'
-const REGISTER_SUCCESS = 'user/REGISTER_SUCCESS'
-export const register = ({ name, email, password, age }) => ({ type: REGISTER, payload: { name, email, password, age } })
-export const registerSaga = createRequestSaga(REGISTER, api.register)
+export const logout = () => ({ type: Types.LOGOUT })
+export const logoutSaga = createRequestSaga(Types.LOGOUT, api.logout)
 
-const UPDATE_USER = 'user/UPDATE_USER'
-const UPDATE_USER_SUCCESS = 'user/UPDATE_USER_SUCCESS'
-export const updateUser = ({ name, password, age }) => ({ type: UPDATE_USER, payload: { name, password, age } })
-export const updateUserSaga = createRequestSaga(UPDATE_USER, api.updateUser)
+export const register = ({ name, email, password, age }) => ({ type: Types.REGISTER, payload: { name, email, password, age } })
+export const registerSaga = createRequestSaga(Types.REGISTER, api.register)
 
-const UPLOAD_AVATAR = 'user/UPLOAD_AVATAR'
-const UPLOAD_AVATAR_SUCCESS = 'user/UPLOAD_AVATAR_SUCCESS'
-export const uploadAvatar = (formData) => ({ type: UPLOAD_AVATAR, payload: formData })
-export const uploadAvatarSaga = createRequestSaga(UPLOAD_AVATAR, api.uploadAvatar)
+export const updateUser = ({ name, password, age }) => ({ type: Types.UPDATE_USER, payload: { name, password, age } })
+export const updateUserSaga = createRequestSaga(Types.UPDATE_USER, api.updateUser)
 
-const REMOVE_USER = 'user/REMOVE_USER'
-export const REMOVE_USER_SUCCESS = 'user/REMOVE_USER_SUCCESS'
-export const removeUser = () => ({ type: REMOVE_USER })
-export const removeUserSaga = createRequestSaga(REMOVE_USER, api.removeUser)
+export const uploadAvatar = (formData) => ({ type: Types.UPLOAD_AVATAR, payload: formData })
+export const uploadAvatarSaga = createRequestSaga(Types.UPLOAD_AVATAR, api.uploadAvatar)
+
+export const removeUser = () => ({ type: Types.REMOVE_USER })
+export const removeUserSaga = createRequestSaga(Types.REMOVE_USER, api.removeUser)
 
 const initialState = {
     user: null
 }
 
 export function* userSaga() {
-    yield takeLatest(GET_USER, getUserSaga)
-    yield takeLatest(LOGIN, loginSaga)
-    yield takeLatest(LOGOUT, logoutSaga)
-    yield takeLatest(REGISTER, registerSaga)
-    yield takeLatest(UPDATE_USER, updateUserSaga)
-    yield takeLatest(UPLOAD_AVATAR, uploadAvatarSaga)
-    yield takeLatest(REMOVE_USER, removeUserSaga)
+    yield all([
+        takeLatest(Types.GET_USER, getUserSaga),
+        takeLatest(Types.LOGIN, loginSaga),
+        takeLatest(Types.LOGOUT, logoutSaga),
+        takeLatest(Types.REGISTER, registerSaga),
+        takeLatest(Types.UPDATE_USER, updateUserSaga),
+        takeLatest(Types.UPLOAD_AVATAR, uploadAvatarSaga),
+        takeLatest(Types.REMOVE_USER, removeUserSaga),
+    ])
 }
 
 function user(state = initialState, action) {
     switch (action.type) {
-        case GET_USER_SUCCESS:
-            return { ...state, user: { ...action.payload, avatar: `${process.env.REACT_APP_API_URL}/user/${action.payload._id}/avatar?d=${new Date().getTime()}` } }
-        case LOGIN_SUCCESS:
-        case REGISTER_SUCCESS:
+        case Types.GET_USER_SUCCESS:
+            return {
+                ...state,
+                user: {
+                    ...action.payload,
+                    avatar: `${process.env.REACT_APP_API_URL}/user/${action.payload._id}/avatar?d=${new Date().getTime()}`
+                }
+            }
+        case Types.LOGIN_SUCCESS:
+        case Types.REGISTER_SUCCESS:
             const { token, user } = action.payload
             window.localStorage.setItem('token', `Bearer ${token}`)
-            return { ...state, user: { ...user, avatar: `${process.env.REACT_APP_API_URL}/user/${user._id}/avatar?d=${new Date().getTime()}` } }
-        case UPDATE_USER_SUCCESS:
-            return { ...state, user: { ...action.payload.data, avatar: `${process.env.REACT_APP_API_URL}/user/${action.payload.data._id}/avatar?d=${new Date().getTime()}` } }
-        case UPLOAD_AVATAR_SUCCESS:
-            return { ...state, user: { ...state.user, avatar: `${process.env.REACT_APP_API_URL}/user/${state.user._id}/avatar?d=${new Date().getTime()}` } }
-        case REMOVE_USER_SUCCESS:
-        case LOGOUT_SUCCESS:
+            return {
+                ...state,
+                user: {
+                    ...user,
+                    avatar: `${process.env.REACT_APP_API_URL}/user/${user._id}/avatar?d=${new Date().getTime()}`
+                }
+            }
+        case Types.UPDATE_USER_SUCCESS:
+            return {
+                ...state,
+                user: {
+                    ...action.payload.data,
+                    avatar: `${process.env.REACT_APP_API_URL}/user/${action.payload.data._id}/avatar?d=${new Date().getTime()}`
+                }
+            }
+        case Types.UPLOAD_AVATAR_SUCCESS:
+            return {
+                ...state,
+                user: {
+                    ...state.user,
+                    avatar: `${process.env.REACT_APP_API_URL}/user/${state.user._id}/avatar?d=${new Date().getTime()}`
+                }
+            }
+        case Types.REMOVE_USER_SUCCESS:
+        case Types.LOGOUT_SUCCESS:
             window.localStorage.removeItem('token')
             return { ...state, user: null }
         default:
