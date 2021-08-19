c5bc2a0a0bc163c4a5dfc48c7f9a405efac7d1d5
Yunseop Kim <yunseop.kim@actionpower.kr>
2021-08-19 11:08:08 +0900

    todo reducer 리팩토링

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,56 +1,61 @@
-import { takeLatest } from 'redux-saga/effects'
+import { all, takeLatest } from 'redux-saga/effects'
 import * as api from '../lib/api'
 import createRequestSaga from '../lib/createRequestSaga'
 
-const GET_TASKS = 'user/GET_TASKS'
-const GET_TASKS_SUCCESS = 'user/GET_TASKS_SUCCESS'
-export const getTasks = () => ({ type: GET_TASKS })
-export const getTasksSaga = createRequestSaga(GET_TASKS, api.getTasks)
+export const Types = {
+    GET_TASKS: 'user/GET_TASKS',
+    GET_TASKS_SUCCESS: 'user/GET_TASKS_SUCCESS',
+    ADD_TASK: 'user/ADD_TASK',
+    ADD_TASK_SUCCESS: 'user/ADD_TASK_SUCCESS',
+    UPDATE_TASK: 'user/UPDATE_TASK',
+    UPDATE_TASK_SUCCESS: 'user/UPDATE_TASK_SUCCESS',
+    REMOVE_TASK: 'user/REMOVE_TASK',
+    REMOVE_TASK_SUCCESS: 'user/REMOVE_TASK_SUCCESS',
+}
 
-const ADD_TASK = 'user/ADD_TASK'
-const ADD_TASK_SUCCESS = 'user/ADD_TASK_SUCCESS'
-export const addTask = ({ description }) => ({ type: ADD_TASK, payload: { description } })
-export const addTaskSaga = createRequestSaga(ADD_TASK, api.addTask)
+export const getTasks = () => ({ type: Types.GET_TASKS })
+export const getTasksSaga = createRequestSaga(Types.GET_TASKS, api.getTasks)
 
-const UPDATE_TASK = 'user/UPDATE_TASK'
-const UPDATE_TASK_SUCCESS = 'user/UPDATE_TASK_SUCCESS'
-export const updateTask = (data) => ({ type: UPDATE_TASK, payload: data })
-export const updateTaskSaga = createRequestSaga(UPDATE_TASK, api.updateTask)
+export const addTask = ({ description }) => ({ type: Types.ADD_TASK, payload: { description } })
+export const addTaskSaga = createRequestSaga(Types.ADD_TASK, api.addTask)
 
-const REMOVE_TASK = 'user/REMOVE_TASK'
-const REMOVE_TASK_SUCCESS = 'user/REMOVE_TASK_SUCCESS'
-export const removeTask = (id) => ({ type: REMOVE_TASK, payload: id })
-export const removeTaskSaga = createRequestSaga(REMOVE_TASK, api.removeTask)
+export const updateTask = (data) => ({ type: Types.UPDATE_TASK, payload: data })
+export const updateTaskSaga = createRequestSaga(Types.UPDATE_TASK, api.updateTask)
+
+export const removeTask = (id) => ({ type: Types.REMOVE_TASK, payload: id })
+export const removeTaskSaga = createRequestSaga(Types.REMOVE_TASK, api.removeTask)
 
 export function* todoSaga() {
-    yield takeLatest(GET_TASKS, getTasksSaga)
-    yield takeLatest(ADD_TASK, addTaskSaga)
-    yield takeLatest(UPDATE_TASK, updateTaskSaga)
-    yield takeLatest(REMOVE_TASK, removeTaskSaga)
+    yield all([
+        takeLatest(Types.GET_TASKS, getTasksSaga),
+        takeLatest(Types.ADD_TASK, addTaskSaga),
+        takeLatest(Types.UPDATE_TASK, updateTaskSaga),
+        takeLatest(Types.REMOVE_TASK, removeTaskSaga),
+    ])
 }
 
 const initialState = {
     count: 0,
-    tasks: null
+    tasks: []
 }
 
 function todo(state = initialState, action) {
     switch (action.type) {
-        case GET_TASKS_SUCCESS:
+        case Types.GET_TASKS_SUCCESS:
             return { ...state, tasks: action.payload.data, count: action.payload.count }
-        case ADD_TASK_SUCCESS:
+        case Types.ADD_TASK_SUCCESS:
             return { ...state, tasks: [...state.tasks, action.payload.data], count: state.count + 1 }
-        case UPDATE_TASK:
+        case Types.UPDATE_TASK:
             return {
                 ...state,
                 tasks: state.tasks.map(task => (task._id === action.payload.id) ? Object.assign(task, action.payload) : task)
             }
-        case UPDATE_TASK_SUCCESS:
+        case Types.UPDATE_TASK_SUCCESS:
             return {
                 ...state,
                 tasks: state.tasks.map(task => task._id === action.payload.data._id ? action.payload.data : task)
             }
-        case REMOVE_TASK_SUCCESS:
+        case Types.REMOVE_TASK_SUCCESS:
             return {
                 ...state,
                 tasks: state.tasks.filter(task => task._id !== action.params)
